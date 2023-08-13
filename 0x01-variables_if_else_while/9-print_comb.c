14bc222a901a86f59dec8006c1bbd87c33e14b1d
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-12 23:13:16 -0700

    base namber

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0.
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0 ; i<= 0 ; i++)
+	{
+		putchar((i % 10) + '0');
+		putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
