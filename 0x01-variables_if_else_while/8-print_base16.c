6cd2a5290bdeb69cb8b88aaa1de9a26735daba76
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-12 22:50:04 -0700

    base namber

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0.
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 0 ; i++)
+		putchar("%x", i);
+	putchar('\n');
+	return (0);
+}
