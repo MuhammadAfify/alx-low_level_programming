b0b152e3603922ecf6e52f0a29f665848dd4367f
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-15 14:00:08 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - checks the sign of the number
+ * @n: the number to check
+ * Return: 1 if n is print_sign, 0 if n is zero
+ * -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+else if (n == 48)
+{
+	_putchar('0')
+	return (0);
+}
+}
