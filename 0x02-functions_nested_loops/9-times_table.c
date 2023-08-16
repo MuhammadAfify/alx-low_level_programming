d0db3abd2b96fc82bb10e6383cb50ee438baea87
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-16 15:54:03 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * , starting with 0
+ */
+void times_table(void)
+{
+	int s, c, r;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		for (r = 0 ; r <= 10 ; r++);
+		{
+		s = c * r;
+		if (s == 0)
+		{
+			_putchar(s + '0');
+		}
+		if (k < 10 && k != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(s + '0');
+		}
+		else if (s >= 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k % 10) + '0');
+			_putchar((k / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
