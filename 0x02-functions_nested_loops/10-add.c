863f2778ee9debb8cd42aecced2d737833252e66
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-15 17:27:33 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,12 @@
  * add - adds two integers and returns the result
  * @x: the first number to add
  * @y: the second number to add
- * Return: sum of a and b
+ * Return: sum of x and y
  */
 
 int add(int x, int y)
 {
-	int x, y, add;
+	int add;
 
 	add = x + y;
 	return (add);
