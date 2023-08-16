90ee31013bf0a2feba1ab3e737205ce89e1f1efe
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-15 19:53:35 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n ; i <= 98 ; i++)
+		for (i = n ; i < 98 ; i++)
 		{
 			printf("%d, ", i);
 			if (i == 98)
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (j = n ; j >= 98 ; j--)
+		for (j = n ; j > 98 ; j--)
 		{
 			printf("%d, ", j);
 			if (j == 98)
