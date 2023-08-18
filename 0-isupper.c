69ca1c14a9be9db8cae64a8ca3c222ffb183baa1
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-17 19:56:08 -0700

    more function

---
+++ 0-isupper.c
@@ -0,0 +1,10 @@
+int "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ * @c : the character that will be checked
+ * Return: returns 1 if it is uppercase , 0 if not
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
