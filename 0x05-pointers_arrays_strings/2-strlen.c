412be15036fa92b443ffdca0eba783f50e1c337a
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-21 14:20:50 -0700

    pointers array strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - the function that get the length of char
+ * @s : the char that will get the length
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+	i	count++;
+		s++;
+	}
+	return (count);
+}
