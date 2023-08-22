1512cb8b90d1335fa24ca3a6f60b7f1583e1489e
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-22 07:50:16 -0700

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - print the phrase
+ *
+ * @str : the pointer that point the phrase
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
