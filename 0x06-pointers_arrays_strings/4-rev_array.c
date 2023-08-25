f799dd6a9815bc0a3c18cb9644c6d4aae59b2f07
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-25 13:20:42 -0700

    pointers array strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content of the array
+ *
+ * @a : the array
+ * @n : the number of content
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp, count;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
