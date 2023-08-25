5a140c6e9128ae31e961f2e1040000ff02a2b2f8
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-24 18:56:45 -0700

    pointers array string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - make lower case letter to upper
+ *
+ * @lower : the string
+ * Return: the upper string.
+ */
+char *string_toupper(char *lower)
+{
+	int x = 0;
+
+	while (lower[x])
+	{
+		if (lower[x] >= 97 && lower[x] <= 122)
+		{
+			lower[x] -= 32;
+		}
+		x++
+	}
+	return (lower);
+}
