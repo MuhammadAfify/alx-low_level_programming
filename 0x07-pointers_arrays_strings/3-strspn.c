af5e907c69dfb1ab9612226492caec6792ddef7d
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-28 16:34:48 -0700

    pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s : the main phrase
+ * @accept : the word will be searched
+ * Return: return the length.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = 0, i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (i = 0; i < p; i++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+			if (s[i] != accept[j])
+			{
+				return (len);
+			}
+		}
+	}
+	return (len);
+}
