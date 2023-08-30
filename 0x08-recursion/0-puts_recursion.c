7a08df2335ef4b1089ba7ff9a5a687466b3749ab
MuhammadAfify <muhammadafefy@gmail.com>
2023-08-30 10:02:31 -0700

    reursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s : the string that will be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+		return ('\n');
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
