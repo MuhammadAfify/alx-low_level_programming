6b3a3815578ad2c2ddd027e08e39ff4433fcba51
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-04 07:52:03 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,19 +8,19 @@
  */
 char *_strdup(char *str)
 {
-	int len = 0;
+	int len = 0, i;
 	char *A;
 
 	while (*(str + 1))
 		len++;
-	if (str == ('\0'))
-		return ('\0');
-	A = malloc(l * sizeof(char) + 1);
+	if (str == 0)
+		return (0);
+	A = malloc(len * sizeof(char) + 1);
 	if (A == 0)
 	{
 		return (0);
 	}
-	for (int i = 0; i <= len; i++)
+	for (i = 0; i <= len; i++)
 		*(A + i) = *(str + i);
 	return (A);
 }
