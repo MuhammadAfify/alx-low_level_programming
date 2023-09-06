1b4cc2874c5cb189d3a3cd31dcb71727a4a5181e
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-06 16:36:47 -0700

    argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ *
+ * argc : the number of paramaters
+ * argv : the pointers of array
+ * Return: always zero.
+ */
+int main(int argc, char **argv)
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
