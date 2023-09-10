f0f1287fc7fe56d9fd05bce281b703f5286b75fc
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-10 08:50:15 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
  * malloc_checked - allocates memory using malloc
  *
  * @b : size of memory
- * Return : return pointer to the address of the block
+ * Return: return pointer to the address of the block
  */
 void *malloc_checked(unsigned int b)
 {
