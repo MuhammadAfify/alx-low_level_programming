5df06dcd713cf02199e7f27fcc735876959aa5c6
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-10 13:37:44 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (result == 0)
 		return (0);
 
-	while (s1[size] != '\0')
+	while (s1[size1] != '\0')
 	{
 		result[len] = s1[size1];
 		len++;
