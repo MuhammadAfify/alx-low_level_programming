cf2579ee7a5e79b5b8c4a7af9b1ed56b9ab369b8
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-11 13:34:45 -0700

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,8 @@
 #ifndef _FUNCTION_LIKE_MACRO
 #define _FUNCTION_LIKE_MACRO
+/**
+ * computes the absolute value of a number x
+ *
+ */
 #define ABS(x) ((x) < 0 ? (x) * (-1) : (X))
 #endif /*_FUNCTION_LIKE_MACRO*/
