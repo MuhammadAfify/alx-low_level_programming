bab47627b09fd31607eab05f9e08c77f98a2467c
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-11 19:41:23 -0700

    struct typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ typedef struct dog
 #define _FUNCTION_H
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
