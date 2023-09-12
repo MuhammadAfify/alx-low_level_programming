7a530b7783992b818e693e44daa7769d2d46e3b8
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-11 18:57:09 -0700

    struct typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,21 @@ void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		if ((d->name) != 0)
+		if ((*d).name != 0)
 		{
-			printf("Name: %s\n", (d->name));
+			printf("Name: %s\n", (*d).name);
 		}
 		else
 			printf("Name: (nil)\n");
-		if ((d->age) > 0)
+		if ((*d).age > 0)
 		{
-			printf("Age: %s\n", (d->age));
+			printf("Age: %s\n", (*d).age);
 		}
 		else
 			printf("Age: (nil)\n");
-		if ((d->owner) != 0)
+		if ((*d).owner != 0)
 		{
-			printf("Owner: %s\n", (d->owner));
+			printf("Owner: %s\n", (*d).owner);
 		}
 		else
 			printf("Owner: (nil)\n");
