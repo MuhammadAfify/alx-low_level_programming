f41fc353cab1876acb760ff2e2cf2d93a10f7d0a
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-11 17:08:32 -0700

    struct typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef STRUCTS
 #define STRUCTS
+/**
+ * struct dog - feature for dog
+ * @name : the name for dog
+ * @age : the age for dog
+ * @owner : name of dog owner
+ */
 typedef struct dog
 {
 	char *name;
