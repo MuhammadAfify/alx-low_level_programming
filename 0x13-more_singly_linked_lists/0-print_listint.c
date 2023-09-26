264fcb1241670c4141feea7d9aeaadc8de186b48
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-25 21:38:38 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - print list of linled list
+ *
+ * @h - pointer to head
+ * Return: no. nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		node++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (node);
+}
