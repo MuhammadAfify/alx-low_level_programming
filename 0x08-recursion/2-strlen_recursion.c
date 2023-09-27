c7b0c327f4243abea811233bb797a3090cc99693
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-26 20:31:15 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion -  length of a string
+ *
+ * @s : the string
+ * Return: return the value of length.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len = len + _strlen_recursion(s + 1);
+	}
+	return (len);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,4 +2,5 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 int factorial(int n);
 void _print_rev_recursion(char *s);
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y;
+int _strlen_recursion(char *s);
