b41b152041be2be345679b5d363a2b26c20e9426
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-26 22:26:33 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
 int pow_opt(int x, int y);
 int _sqrt_recursion(int n);
+int divide_available(int num, int div);
+int is_prime_number(int n);
