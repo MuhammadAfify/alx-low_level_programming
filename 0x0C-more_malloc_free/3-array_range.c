fbe86cc818808c6149e6e83abc7b355b217e1c7e
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-27 06:51:08 -0700

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min +1;
+	size = max - min + 1;
 	new_array = malloc(sizeof(int) * size);
 	if (new_array == NULL)
 		return (NULL);
