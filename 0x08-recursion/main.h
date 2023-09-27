8f38578b6980493f4cb39c6d619bbc701806b5c2
MuhammadAfify <muhammadafefy@gmail.com>
2023-09-26 22:14:52 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,5 @@ int factorial(int n);
 void _print_rev_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
+int pow_opt(int x, int y);
+int _sqrt_recursion(int n);
