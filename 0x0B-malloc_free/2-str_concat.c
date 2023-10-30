325f1cc74808517a71f4010c1e8e1f7da116ebb7
MuhammadAfify <muhammadafefy@gmail.com>
2023-10-30 12:29:20 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,21 +9,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1 = 0,size2 = 0, i;
+	int size1 = 0, size2 = 0, i;
 	char *arr;
 
 	if (s1 == 0)
-		return ('\0');
+		s1 = "";
 	if (s2 == 0)
-		return ('\0');
+		s2 = "";
 	while (s1[size1] != 0)
 		size1++;
 	while (s2[size2] != 0)
 		size2++;
-	arr = malloc(((size1 + size2) * sizeof(char) +1));
+	arr = malloc(((size1 + size2) * sizeof(char) + 1));
 	if (arr == 0)
 		return ('\0');
-	for (i = 0; i < (size1 + size2); i++)
+	for (i = 0; i <= (size1 + size2); i++)
 	{
 		if (size1 < i)
 			arr[i] = s1[i];
