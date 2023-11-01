cda5ce15aa7fcedb47d85398cbc4d4dd4ee56153
MuhammadAfify <muhammadafefy@gmail.com>
2023-10-31 19:04:45 -0700

    malloc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main func
+ *
+ * @argc: the no. elements
+ * @argv: the array of content
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n",argv[i]);
+	}
+	return (0);
+}
