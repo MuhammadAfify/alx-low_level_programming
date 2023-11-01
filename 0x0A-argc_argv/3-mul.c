c74a80b88353721889bcc59cee75294c6b674783
MuhammadAfify <muhammadafefy@gmail.com>
2023-10-31 19:17:22 -0700

    malloc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
+	int mul;
+
 	if (argc != 3)
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 	}
-	printf("%d", atoi(argv[1]) * atoi(argv[2]));
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i", mul);
 	return (0);
 }
