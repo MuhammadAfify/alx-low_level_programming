ace805dad01d2ccc75d3138a7fbc5c4163b6c766
MuhammadAfify <muhammadafefy@gmail.com>
2023-10-31 18:39:25 -0700

    malloc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * @argv: the array
  * Return: Always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	if (argc > 0)
-		printf("%s\n", argv[0]);
+		printf("%d\n", argv[0]);
 	return (0);
 }
