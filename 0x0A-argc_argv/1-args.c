d92d43b4ec1a6307a4e57e779c64732b3504aa67
MuhammadAfify <muhammadafefy@gmail.com>
2023-10-31 18:58:30 -0700

    malloc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: the array  
  * Return: Always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%i\n", (argc - 1));
 	return (0);
