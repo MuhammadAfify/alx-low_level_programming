5107a277ce6d2e9391c931cd4eed935d69459e37
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-03 11:26:19 -0700

    malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,6 +15,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	array = malloc(sizeof(int) * (max - min + 1));
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; min <= max; min++, i++)
 	{
 		array[i] = min;
