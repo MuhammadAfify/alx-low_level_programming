db438d2fe5958749cf052110d3cc4d1b029065b9
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-03 11:04:03 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	new_array = malloc(nmemb * size);
 	if (new_array == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb + size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 		new_array[i] = 0;
 	return (new_array);
 }
