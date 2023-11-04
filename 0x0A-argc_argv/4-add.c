8944064f54619346282dc5fdb53f267fa64e6fcd
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-03 23:29:51 -0700

    argc argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main function to get add
+ *
+ * @argc: no. arguments
+ * @argv: pointers to the arguments
+ * Return: return the add
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0, i, j;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != 0; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add = add + atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
