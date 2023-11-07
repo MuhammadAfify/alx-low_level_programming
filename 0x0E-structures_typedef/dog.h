6adc1623ccd74c04e16953bb472aff94407d16c1
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-07 15:11:32 -0800

    structures typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - feature for dog
+ * @name : the name for dog
+ * @age : the age for dog
+ * @owner : name of dog owner
+ */
+typedef struct dog
+{
+    char *name;
+    float *age;
+    char *owner;
+}
+#endif /*STRUCTS*/
