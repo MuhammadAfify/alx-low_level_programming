9540b3c5fcb2d88012f883db91267d5d4a6594be
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-07 15:28:49 -0800

    structures typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print the dog data
+ * @d : the struct of dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if ((d->name) != 0)
+		{
+			printf("Name: %s\n", (d->name));
+		}
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %s\n", (d->age));
+		if ((d->owner) != 0)
+		{
+			printf("Owner: %s\n", (d->owner));
+		}
+		else
+			printf("Owner: (nil)\n");
+	}
+}
