977333aae616ff181939bb5f37acb0803b4497b8
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-07 15:38:36 -0800

    structures typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d : pointer of struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
