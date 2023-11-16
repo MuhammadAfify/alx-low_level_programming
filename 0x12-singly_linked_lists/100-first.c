464b43a058f7765e4d160a066a1994af4d9c3f13
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-16 04:15:06 -0800

    singly linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * print_msg  - print message
  *
  */
-void print_msg(void)
+void __attribute__((constructor)) print_msg(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
