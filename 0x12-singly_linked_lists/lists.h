5125436b9477a5ca4af47cfebad553a5ad9716c9
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-15 22:03:11 -0800

    singly linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - single linkedlist
+ * @str: string
+ * @len: length of string
+ * @next: pointers for next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /*LIST_H*/
