faba0cdbc4e605d2ac47c69d8d2811ca2d7c5f4f
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-15 22:16:42 -0800

    singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - no. elements in linked list
+ * 
+ * @h : linkedlist
+ * Return: no. elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elem = 0;
+
+	while (h)
+	{
+		elem++;
+		h = h->next;
+	}
+	return (elem);
+}
