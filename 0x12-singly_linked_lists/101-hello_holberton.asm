d1921d673306bc96f24d6725ea2b57f3f8c0a112
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-17 03:27:16 -0800

    singly linked list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,22 @@
+extern printf
+
 section .text
-   extern printf
    global main
 
 main:
-   mov rsi,msg
+   push rbp
+
+   mov edi,fmt
+   mov eax,msg
    mov rax,0
    call printf
 
+   pop rbp
+
+   mov rax,0
+   ret
+
 section .data
-   msg db 'Hello, Holberton', 0xa, 0
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
 
