d2493a03713cae0b9832cb75cf4f0cd3afa2c8c3
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-18 04:20:27 -0800

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * square_find - find the natuaral square
+ *
+ * @x : the number
+ * @y : iterator
+ * Return: square root
+ */
+int square_find(int x, int y)
+{
+	if (y % (x / y) == 0)
+	{
+		if (y * (x / y) == x)
+			return (y);
+		else
+			return (-1);
+	}
+	return (0 + pow_opt(x, y + 1));
+}
+/**
+ * _sqrt_recursion - natural square root of a number
+ *
+ * @n : the number
+ * Return: natural root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (square_find(n , 2));
+}
