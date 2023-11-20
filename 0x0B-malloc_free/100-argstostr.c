42b4d88516561087b41417a9651a2d3902d0712f
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-19 23:06:30 -0800

    malloc free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments
+ *
+ * @ac: integer type
+ * @av: array pointer
+ * Return: pointer of argument
+ */
+char *argstostr(int ac, char **av)
+{
+	int size, i, count1, count2 = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (count1 = 0; av[i][count1] != '\0'; count1++)
+		{
+			size += 1;
+		}
+		size += 1;
+	}
+	size += 1;
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (count1 = 0; av[i][count1] != '\0'; count1++)
+		{
+			ptr[count2] = av[i][count1];
+			count2++;
+		}
+		ptr[count2] = '\n';
+		count2++;
+	}
+	ptr[count2] = '\0';
+	return (ptr);
+}
