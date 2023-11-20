db5651820c5282468587b31398f5642c16ad8659
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-20 13:59:18 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 	
-	while (*head)
+	while (head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = (head)->next;
+		free(head);
+		head = temp;
 	}
 	head = NULL;
 }
