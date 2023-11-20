fef55c2a0067b658a3614c5997c1628d1e2f5ecc
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-20 13:57:19 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
+ * free_listint - free linked list
  *
+ * @head: pointers to head
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-
-	if (head == NULL)
-		return;
+	
 	while (*head)
 	{
 		temp = (*head)->next;
