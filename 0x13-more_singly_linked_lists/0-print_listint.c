c5c81469f3acd4e40bdad4ede5e59e68e81099ca
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-20 13:23:46 -0800

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements
+ *
+ * @h: pointer to head
+ * Return: numbers of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+        size_t node;
+
+        for (node = 0; h; node++)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
+        return (node);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this more singly linked lists task readme
