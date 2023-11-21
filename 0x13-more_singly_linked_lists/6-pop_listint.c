94b62bff278da16e2d62f2d364aa545b352b5466
MuhammadAfify <muhammadafefy@gmail.com>
2023-11-20 23:42:29 -0800

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - delete head of node
+ *
+ * @head: pointers of head
+ * Return: head of node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int new_head;
+
+	if (*head == NULL)
+		return NULL;
+	temp = *head;
+	new_head = (*head)->n;
+	*head = (*head)->next;
+	free (temp);
+	return (new_head);
+}
