043394181bb8d382f74eaad15a70ac7b548cd4b2
MuhammadAfify <muhammadafefy@gmail.com>
2024-01-11 12:44:45 -0800

    doubly linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,9 +8,9 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	while(index != 0)
+	while (index != 0)
 	{
-		if(head == NULL)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 		index--;
