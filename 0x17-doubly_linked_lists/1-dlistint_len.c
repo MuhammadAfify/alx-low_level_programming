8670d710260dd32a8918be5a87ac0e1981d2031d
MuhammadAfify <muhammadafefy@gmail.com>
2024-01-11 12:04:16 -0800

    doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
+ * dlistint_len - count number of elements
  *
- *
+ * @h: pointer to node
+ * Return: return number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
