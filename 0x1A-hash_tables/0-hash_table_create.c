917bb4b5d4335ac9f1a5b2a5db894c9027737804
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-02 14:10:23 -0800

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	if (size == NULL)
+	if (size == 0)
 		return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
