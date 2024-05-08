251ee1396a3b364b21d9754f84b8085dd04834f8
MuhammadAfify <muhammadafefy@gmail.com>
2024-05-08 15:22:47 -0700

    Search Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,24 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
+
+#endif/*SEARCH_ALGOS*/
